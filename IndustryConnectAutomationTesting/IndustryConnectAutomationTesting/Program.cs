89ebe1112b7e94c373bab4bbd7d4bf6dbed15912
KanjulaGunathilaka <129904620+KanjulaGunathilaka@users.noreply.github.com>
2023-04-06 18:46:21 +1200

    Add files via upload

---
+++ IndustryConnectAutomationTesting/IndustryConnectAutomationTesting/Program.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+// open chrome browser
+IWebDriver driver = new ChromeDriver();
+driver.Manage().Window.Maximize();
+// launch turn up portal
+driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+// Identify username textbox and enter valid username
+IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
+usernameTextbox.SendKeys("hari");
+// Identify password textbox and enter valid password
+IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
+passwordTextbox.SendKeys("123123");
+// identify logging button and click on it
+IWebElement loggingButton = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
+loggingButton.Click();
+// check if user has logged succussfully
+IWebElement helloHari = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
+
+if (helloHari.Text == "helloHari!")
+{
+    Console.WriteLine("user has logged succussfully.");
+}
+else
+{
+    Console.WriteLine("user hasn't been logged in.");
+}
+
+driver.Quit();
+
